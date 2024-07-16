562914a454ff9dad0ff4199c95eabb74a4ee5f64
mayurkukade <mayurkukade@opticalarc.com>
2024-07-16 23:12:28 +0530

    update

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const connectDB = require('./config/connectDB')
+const app = express()
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+connectDB()
+
+app.use('/',require('./router/root'))
+
+mongoose.connection.once('open',()=>{
+    console.log('mongodb is connected')
+    app.listen(PORT,()=>{
+       console.log(`server is running on ${PORT}`)
+    })
+})
+
+mongoose.connection.on('err',()=>{
+    console.log(err)
+})
+
